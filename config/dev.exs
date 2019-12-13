5f5957cfb8b44d6702750869f6554ea7ed772954
GlennGeelen <g.geelen@defacto.nl>
2019-12-13 08:48:47 +0100

    Remove javascript it is not used

--- config/dev.exs
+++ config/dev.exs
@@ -11,15 +11,7 @@ config :certify_cards, CertifyCardsWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
