655c8ca4507bea4290cbde92ec3b7ff53f239e8b
GlennGeelen <g.geelen@defacto.nl>
2019-12-13 09:06:40 +0100

    remove cache_static_manifest because we dont have assets

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,7 @@ use Mix.Config
 config :certify_cards, CertifyCardsWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "certify-cards", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
